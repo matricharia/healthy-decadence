469d126905bcbb77e02bda28c43f6acac5956958
Laila Rodriguez <matricharia@gmail.com>
2016-02-14 21:38:13 +0000

    Generated model for recipe database

---
+++ db/migrate/20160214213334_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+
+    	t.string :tittle
+    	t.string :instructions
+    	t.string :author
+
+      t.timestamps
+    end
+  end
+end
