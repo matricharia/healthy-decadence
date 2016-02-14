0bde020208e3a13217bd76c440a7e540717a2db7
Laila Rodriguez <matricharia@gmail.com>
2016-02-14 19:28:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthyDecadence::Application.config.secret_key_base = 'f8ef2d8d5d0fcb5756585e2220f00653d31ca057e6dc89a8f04f0a210dd79b6a8c10a42106458184da80d954642e2f6c313caac879979d9fe63ce977d13209f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HealthyDecadence::Application.config.session_store :cookie_store, key: '_healthy-decadence_session'
